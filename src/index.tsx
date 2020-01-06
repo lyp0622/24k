1b37f5cb149e393c8c78392e3598898674375a4f
lyp <1183391880@qq.com>
2020-01-06 13:43:56 +0800

    router

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './router/index';
+import StoreContext from './context/StoreContext'
+import store from './store/index'
+
+ReactDOM.render(<StoreContext.Provider value={store}>
+        <App />
+  </StoreContext.Provider>,document.getElementById('root'));
